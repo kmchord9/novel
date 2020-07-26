d364a79dc4c239809d67dcf8b896218c1d90fcc8
kmchord9 <kmchord9@gmial.com>
2020-07-26 09:24:57 +0900

    first commit

---
+++ novel.py
@@ -0,0 +1,77 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib
+import time
+import os
+
+'''
+なろう小説のテキストファイルを生成
+・保存するファイル名をnovel_titleに設定する
+・ノベルのページのURLをnovel_urlに設定する
+連番でURLを探索するのでページが見つからない場合に自動で停止する
+
+例：
+novel_title = "乙女ゲームの破滅フラグしかない悪役令嬢に転生してしまった"
+novel_url = "https://ncode.syosetu.com/n5040ce/"
+'''
+
+#保存時のファイルタイトル
+novel_title = "デスマーチからはじまる異世界狂想曲"
+
+#取得ページURL
+novel_url = "https://ncode.syosetu.com/n9902bn/"
+
+file_path = os.path.dirname(__file__)
+save_path = file_path + novel_title + ".txt"
+
+#初期ページナンバー
+page_number = 1
+
+with open(save_path, mode='w') as f:
+    while True:
+        all_txt = []
+        try:
+            url = novel_url+str(page_number)
+            html = urllib.request.urlopen(url)
+            soup = BeautifulSoup(html, "html.parser")
+
+        except OSError as e:
+          print("html not found \n  scraping finished")
+          break
+
+        except Exception as e:
+          print(e)
+          break
+
+        #タイトルナンバー
+        title_no_html = soup.find("div", id="novel_no")
+        title_no = title_no_html.string + "\n"
+        all_txt.append(title_no)
+
+        #タイトル抽出
+        title_html = soup.find("p", class_="novel_subtitle")
+        title = title_html.string + "\n"
+        all_txt.append(title)
+
+        #本文抽出
+        honbun = soup.find("div", id="novel_honbun").find_all("p")
+        for txt in honbun:
+          if txt.string == None:
+            all_txt.append("\n")
+          else:
+            all_txt.append(txt.string)
+        
+        #本文結合
+        data_txt = ''.join(all_txt)
+
+        #本文ファイル書き込み
+        f.write(data_txt)
+
+        #進行ログ
+        print('{}:ページ'.format(page_number))
+
+        #次ページナンバー
+        page_number +=1 
+
+        #処理遅延(web負荷低減)
+        time.sleep(0.5)
