ac86b71c60904bf0f2fa87a3500122a622585b17
kmchord9 <kmchord9@gmial.com>
2021-01-09 20:22:52 +0900

    なろう小説対応修正

--- novel.py
+++ novel.py
@@ -12,20 +12,27 @@ import os
 
 例：
 novel_title = "乙女ゲームの破滅フラグしかない悪役令嬢に転生してしまった"
-novel_url = "https://ncode.syosetu.com/n5040ce/"
+novel_url = "https://ncode.syosetu.com/n4830bu/"
 '''
 
 #保存時のファイルタイトル
-novel_title = ""
+novel_title = "honnsukino"
 
 #取得ページURL
-novel_url = ""
+novel_url = "https://ncode.syosetu.com/n4830bu/"
 
 file_path = os.path.dirname(__file__)
 save_path = file_path + novel_title + ".txt"
 
+num_table = str.maketrans('1234567890','１２３４５６７８９０')
+
 #初期ページナンバー
 page_number = 1
+
+def addHeading(letNum, title):
+  num_str = str(letNum)
+  txt = '［＃{letNum}字下げ］{title}［＃「{title}」は中見出し］'.format(letNum=num_str.translate(num_table), title=title )
+  return "\n\n" + txt +"\n\n"
 
 with open(save_path, mode='w') as f:
     while True:
@@ -44,13 +51,14 @@ with open(save_path, mode='w') as f:
           break
 
         #タイトルナンバー
-        title_no_html = soup.find("div", id="novel_no")
-        title_no = title_no_html.string + "\n"
-        all_txt.append(title_no)
+        #title_no_html = soup.find("div", id="novel_no")
+        #title_no = title_no_html.string + "\n"
+        #all_txt.append(title_no)
 
         #タイトル抽出
         title_html = soup.find("p", class_="novel_subtitle")
-        title = title_html.string + "\n"
+        #title = title_html.string + "\n"      
+        title = addHeading(1, title_html.string)
         all_txt.append(title)
 
         #本文抽出
